5b2c4a5602e45091fa3f1f5ceb4c023527327ddc
SPlakh2201 <88394110+SPlakh2201@users.noreply.github.com>
2022-02-11 19:28:46 +0300

    Update task_8.js

--- Lesson_2/task_8.js
+++ Lesson_2/task_8.js
@@ -1,1 +1,12 @@
+function power(val, pow) {
+    if (pow == 1) {
+        return val;
+    } else {
+        return val * power(val, pow - 1);
+    }
+}
 
+var val = prompt("Введите число.");
+var pow = prompt("Введите степень.");
+
+alert("Result: " + power( Number(val), Number(pow)));
