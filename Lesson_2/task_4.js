b54b849184ff8b05dfdb2a8fd013eb9f0cde4493
SPlakh2201 <88394110+SPlakh2201@users.noreply.github.com>
2022-02-11 16:59:54 +0300

    Create task_4.js

---
+++ Lesson_2/task_4.js
@@ -0,0 +1,98 @@
+const a = prompt('Введите число в промежутке [0, 15].');
+switch (a) {
+    case '0':
+        for(let i = Number(a); i <= 15; i++) {
+            console.log(i);
+        }
+        break;
+
+    case '1':
+        for(let i = Number(a); i <= 15; i++) {
+            console.log(i);
+        }
+        break;
+    
+    case '2':
+        for(let i = Number(a); i <= 15; i++) {
+            console.log(i);
+        }
+        break;
+
+    case '3':
+        for(let i = Number(a); i <= 15; i++) {
+            console.log(i);
+        }
+        break;
+    
+    case '4':
+        for(let i = Number(a); i <= 15; i++) {
+            console.log(i);
+        }
+        break;
+
+    case '5':
+        for(let i = Number(a); i <= 15; i++) {
+            console.log(i);
+        }
+        break;
+    
+    case '6':
+        for(let i = Number(a); i <= 15; i++) {
+            console.log(i);
+        }
+        break;
+    
+    case '7':
+        for(let i = Number(a); i <= 15; i++) {
+            console.log(i);
+        }
+        break;
+
+    case '8':
+        for(let i = Number(a); i <= 15; i++) {
+            console.log(i);
+        }
+        break;
+
+    case '9':
+        for(let i = Number(a); i <= 15; i++) {
+            console.log(i);
+        }
+        break;
+
+    case '10':
+        for(let i = Number(a); i <= 15; i++) {
+            console.log(i);
+        }
+        break;
+
+    case '11':
+        for(let i = Number(a); i <= 15; i++) {
+            console.log(i);
+        }
+        break;
+
+    case '12':
+        for(let i = Number(a); i <= 15; i++) {
+            console.log(i);
+        }
+        break;
+
+    case '13':
+        for(let i = Number(a); i <= 15; i++) {
+            console.log(i);
+        }
+        break;
+
+    case '14':
+        for(let i = Number(a); i <= 15; i++) {
+            console.log(i);
+        }
+        break;
+
+    case '15':
+        for(let i = Number(a); i <= 15; i++) {
+            console.log(i);
+        }
+        break;
+}
