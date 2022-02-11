0165ec9760a8db290db5b1c226384b4503b2f10e
SPlakh2201 <88394110+SPlakh2201@users.noreply.github.com>
2022-02-11 18:48:55 +0300

    Update task_6.js

--- Lesson_2/task_6.js
+++ Lesson_2/task_6.js
@@ -25,4 +25,4 @@ var a = prompt("Введите 1-ое число.");
 var operation = prompt("Введите операцию(+, -, *, /).");
 var b = prompt("Введите 2-ое число.");
 
-alert("Result: " + mathOperation( Number(a), Number(b), operation))
+alert("Result: " + mathOperation( Number(a), Number(b), operation));
