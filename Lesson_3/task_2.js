1fce206487ed24a0ee47588af76a15c0684a3236
SPlakh2201 <88394110+SPlakh2201@users.noreply.github.com>
2022-02-11 23:00:11 +0300

    Update task_2.js

--- Lesson_3/task_2.js
+++ Lesson_3/task_2.js
@@ -3,15 +3,15 @@ var cart = [];
 
 for (let i = 0; i < count; i++) {
     let product = [];
-    product.push( prompt("Введите название товара."));
     product.push( prompt("Введите ID товара."));
     product.push( prompt("Введите стоимость товара."));
+    product.push( prompt("Введите количество товара."));
     cart.push(product);
 }
 
 var sum = 0;
 
 for (let i = 0; i < cart.length; i++) {
-    sum += Number(cart[i][2]);
+    sum += Number(cart[i][2]) * Number(cart[i][1]);
 }
 alert("Итого: " + sum);
